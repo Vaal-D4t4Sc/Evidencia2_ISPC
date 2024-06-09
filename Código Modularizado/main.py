728142fb2850cf9c74421dc3dcbc11ac4387af25
Lucas Ledesma <lucas.dylan.ledesma@gmail.com>
2024-06-09 19:46:24 -0300

    Add files via upload

---
+++ Código Modularizado/main.py
@@ -0,0 +1,7 @@
+#MAIN: El punto de entrada que llama a la función mostrar_menu() del módulo menu.py.
+
+from menu import mostrar_menu
+
+if __name__ == "__main__":
+    mostrar_menu()
+
---
+++ Código Modularizado/usuario.py
@@ -0,0 +1,13 @@
+#USUARIO: Contiene las funciones iniciar_sesion() y registrarse() para manejar las operaciones de usuario.
+
+def iniciar_sesion():
+    usuario = input("Ingrese su nombre de usuario: ")
+    contrasena = input("Ingrese su contraseña: ")
+    print("Iniciando sesión...")
+
+def registrarse():
+    nombre = input("Ingrese su nombre completo: ")
+    usuario = input("Ingrese su nombre de usuario: ")
+    contrasena = input("Ingrese su contraseña: ")
+    email = input("Ingrese su correo electrónico: ")
+    print("Registrándose...")
