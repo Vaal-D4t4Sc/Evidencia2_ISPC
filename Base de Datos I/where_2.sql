ec560e2a73b089e196fcab8b4f9389ed8bacb95c
Lucas Ledesma <lucas.dylan.ledesma@gmail.com>
2024-06-14 03:01:14 -0300

    Update where_2.sql

--- Base de Datos I/where_2.sql
+++ Base de Datos I/where_2.sql
@@ -1,1 +1,2 @@
-SELECT Song.* FROM Song JOIN Album ON Song.AlbumID = Album.AlbumID JOIN Artist ON Album.ArtistID = Artist.ArtistID WHERE Artist.Name = 'Bob Marley';  
+SELECT Song.* FROM Song JOIN Album ON Song.AlbumID = Album.AlbumID JOIN Artist 
+  ON Album.ArtistID = Artist.ArtistID WHERE Artist.Name = 'Bob Marley';  
