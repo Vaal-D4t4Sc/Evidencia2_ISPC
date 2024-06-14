22ca50fed1020fce32cec20a96b6bf67cb9485ef
Lucas Ledesma <lucas.dylan.ledesma@gmail.com>
2024-06-14 02:05:21 -0300

    Update where_inner join.sql

--- Base de Datos I/where_inner join.sql
+++ Base de Datos I/where_inner join.sql
@@ -2,4 +2,4 @@ SELECT *
 FROM song 
 INNER JOIN album ON song.albumID = album.albumID 
 INNER JOIN artist ON album.artistID = artist.artistID 
-WHERE album.name = 'Led Zeppellin'; 
+WHERE album.name = 'Led Zeppelin'; 
