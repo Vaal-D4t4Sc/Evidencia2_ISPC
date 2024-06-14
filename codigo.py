8cd0f52cfb330fac6c2f274028e20a3fa4c00f5a
Valentina Pich <169078817+Vaal-D4t4Sc@users.noreply.github.com>
2024-06-14 08:32:03 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -55,7 +55,8 @@ def mostrar_ayuda():
     print("1. Para iniciar sesión, seleccione la opción 1 y siga las instrucciones.")
     print("2. Para registrarse, seleccione la opción 2 y complete el formulario.")
     print("3. Para explorar música, seleccione la opción 3.")
-    print("4. Si necesita más ayuda, visite nuestro sitio web o contacte a soporte.")
+    print("4. Para crear una playlist, seleccione la opción 4.")
+    print("5. Si necesita más ayuda, visite nuestro sitio web o contacte a soporte.")
     print("==================================")
     input("Presione cualquier tecla para volver al menú principal...")
 
