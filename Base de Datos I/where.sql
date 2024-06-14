eeac51939a079ff7d6b9afd4344a6789cca21925
Lucas Ledesma <lucas.dylan.ledesma@gmail.com>
2024-06-14 01:39:02 -0300

    Update where.sql

--- Base de Datos I/where.sql
+++ Base de Datos I/where.sql
@@ -1,9 +1,1 @@
-SELECT * FROM Album
-WHERE ArtistID = (SELECT ArtistID FROM Artist WHERE Name = 'Bob Marley');
-SELECT Song.*
-FROM Song
-JOIN Album ON Song.AlbumID = Album.AlbumID
-JOIN Artist ON Album.ArtistID = Artist.ArtistID
-WHERE Artist.Name = 'Bob Marley';
-SELECT * FROM Playlist
-WHERE UserID = (SELECT UserID FROM User WHERE Username = 'user1');
+SELECT * FROM Album WHERE ArtistID = ( SELECT ArtistID FROM Artist WHERE Name ='Bob Marley' ); 
